3800b186bcbfddefd0d12a46d67ed035175f7d22
Shira <duydongoc1996@gmail.com>
2024-07-17 13:25:05 +0700

    fix hydration error

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -1,22 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [currentDateTime, setCurrentDateTime] = useState(
-    new Date().toLocaleString()
-  );
+  const [currentDateTime, setCurrentDateTime] = useState("");
 
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentDateTime(new Date().toLocaleString());
     }, 1000);
-
     return () => clearInterval(timer);
   }, []);
 
   return (
     <div className="navbar bg-base-100 flex justify-between">
       <a className="btn btn-ghost text-xl">La Coco Crypto Exchange</a>
-      <span>{currentDateTime}</span>
+      {/* <span>{currentDateTime}</span> */}
     </div>
   );
 }
